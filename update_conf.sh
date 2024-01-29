4f293e760c216aa455bf5d8ec8cfc6c5467f58ed
YuriChan09 <78018797+YuriChan09@users.noreply.github.com>
2024-01-30 01:46:00 +0800

    Create update_conf.sh

---
+++ update_conf.sh
@@ -0,0 +1,19 @@
+cat << EOF >> ~/.bashrc
+pal_config=/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
+default_pal_config=/home/steam/Steam/steamapps/common/PalServer/DefaultPalWorldSettings.ini 
+
+# modify PalWorldSettings.ini
+update-pal-config() {
+    local param=\$1
+    local value=\$2
+    sed -i "s/\(\$param=\)[^,]*/\1\$value/" "\$pal_config"
+}
+EOF
+
+
+# 服务器配置文件没有内容，需要自行复制一份默认的配置文件供其使用
+# 注意：尽管下面的cp命令使用了强制覆盖-f，但是系统可能会通过设置alias cp='cp -i'强制提示是否确认覆盖，遇到是否要覆盖的提示的话，输入y加回车确认
+
+cp -f $default_pal_config $pal_config
+
+source ~/.bashrc
